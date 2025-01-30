a3cec8a62503fefb1de53de0d07664eb6068cc7c
Thom Gusterson <thom@proworkflow.com>
2025-01-30 19:40:01 +1300

    wip easier linter install

---
+++ lua/config/ensure_installed.lua
@@ -0,0 +1,17 @@
+return {
+  servers = {
+    "lua_ls",
+    "tsserver",
+    "jsonls",
+    "marksman",
+  },
+  formatters = {
+    "stylua",
+    "prettierd",
+    "markdownlint",
+    "eslint_d",
+    "luacheck",
+    "htmlhint",
+  },
+  linters = {}
+}
