6b526b7b8a3e60b9414e94e725ea6762771b3ef9
Thom Gusterson <thom@proworkflow.com>
2025-02-08 11:59:06 +1300

    update oil keybind

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,14 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	lazy = false,
 }
